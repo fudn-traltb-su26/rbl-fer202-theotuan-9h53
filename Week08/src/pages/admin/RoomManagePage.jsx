641e8d5a2288b31622add9e416b511eb486aa750
nguyenkhoi190305-cell <nguyenkhoi190305@gmail.com>
2026-07-21 21:57:51 +0700

    Add Bao cao nhom W8

--- Week08/src/pages/admin/RoomManagePage.jsx
+++ Week08/src/pages/admin/RoomManagePage.jsx
@@ -303,14 +303,6 @@ const RoomManagePage = () => {
       </Modal>
 
     </Container>
-
-const RoomManagePage = () => {
-  return (
-    <div className="container mt-5">
-      <h2>Quản lý phòng</h2>
-      <p>Trang dành cho admin để thêm, sửa, xóa phòng.</p>
-    </div>
-
   );
 };
 
